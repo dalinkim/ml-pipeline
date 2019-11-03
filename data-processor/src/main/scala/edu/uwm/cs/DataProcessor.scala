686f7b0b716541a437a48d7eafd4568802e7243f
Dalin Kim <dalinkim7@gmail.com>
2019-11-03 08:07:06 -0600

    v0.2.2: updated path names

--- data-processor/src/main/scala/edu/uwm/cs/DataProcessor.scala
+++ data-processor/src/main/scala/edu/uwm/cs/DataProcessor.scala
@@ -7,14 +7,14 @@ import org.apache.spark.sql.DataFrame
 object DataProcessor {
 
   private val s3Prefix = "s3://"
-  private val s3BucketName = "dalin-ml-pipeline/"
-  private val indexTablePath = "input/index_table/"
+  private val s3BucketName = "dalin-ml-pipeline"
+  private val indexTablePath = "input/index_table"
 
-  private val dataSourcePath = s"$s3Prefix$s3BucketName" + "input/NIS_2016_Core.ASC"
-  private val diagnosisIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "diagnosis_index/*.csv"
-  private val externalCauseIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "external_cause_index/*.csv"
-  private val procedureIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "procedure_index/*.csv"
-  private val dataOutputPath = s"$s3Prefix$s3BucketName" + "input/transformed"
+  private val dataSourcePath = s"$s3Prefix$s3BucketName" + "/input/NIS_2016_Core.ASC"
+  private val diagnosisIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "/diagnosis_index/*.csv"
+  private val externalCauseIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "/external_cause_index/*.csv"
+  private val procedureIndexTablePath = s"$s3Prefix$s3BucketName$indexTablePath" + "/procedure_index/*.csv"
+  private val dataOutputPath = s"$s3Prefix$s3BucketName" + "/input/transformed"
 
   def main(args: Array[String]): Unit = {
 
