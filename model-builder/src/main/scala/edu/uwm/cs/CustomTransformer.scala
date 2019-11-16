877c0107469afd3c9b87d5ad1d3a64f0f97e49bd
Dalin Kim <dalinkim7@gmail.com>
2019-11-16 08:46:14 -0600

    v0.3.1: added to satisfy strict libsvm format requirement for XGBoostSageMakerEstimator

---
+++ model-builder/src/main/scala/edu/uwm/cs/CustomTransformer.scala
@@ -0,0 +1,30 @@
+package edu.uwm.cs
+
+import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.param.{Param, ParamMap}
+import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.types.StructType
+
+/**
+ * This customer transformer exists to transform a DataFrame to only have label and features columns
+ * to resolve libsvm exception thrown by XGBoostSageMakerEstimator when input DataFrame contains more than these columns.
+ *
+ * See <a href="https://github.com/aws/sagemaker-spark/issues/47">https://github.com/aws/sagemaker-spark/issues/47</a>
+ */
+class CustomTransformer(override val uid: String) extends Transformer {
+  final val inputCol: Param[String] = new Param[String](this, "inputCol", "input column name")
+  final def getInputCol: String = $(inputCol)
+  final def setInputCol(value: String): CustomTransformer = set(inputCol, value)
+
+  final val outputCol: Param[String] = new Param[String](this, "outputCol", "output column name")
+  final def getOutputCol: String = $(outputCol)
+  final def setOutputCol(value: String): CustomTransformer = set(outputCol, value)
+
+  def copy(extra: ParamMap): CustomTransformer = { defaultCopy(extra) }
+
+  override def transformSchema(schema: StructType): StructType = { schema }
+
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    dataset.select("label","features")
+  }
+}
